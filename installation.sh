007c3e994605e1d24e652602f5cbd448e23c316e
Nurbolat <nurbolat.meshitbay@gmail.com>
2024-03-05 13:55:11 +0500

    fix

--- installation.sh
+++ installation.sh
@@ -1,6 +1,7 @@
+cp .env.example .env
 docker compose exec -T laravel.test  chmod -R 777 storage/ bootstrap/
 docker compose exec -T laravel.test  cp .env.example .env
 docker compose exec -T laravel.test  composer install
 docker compose exec -T laravel.test  php artisan migrate
 docker compose exec -T laravel.test  php artisan db:seed
-cd vue && sudo chmod -R 777 . &&cp .env.example .env
+cd vue && sudo chmod -R 777 . && cp .env.example .env
