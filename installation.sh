0b90cdc510b83776d7966771888afc16ba3baa67
Nurbolat <nurbolat.meshitbay@gmail.com>
2024-03-05 14:20:18 +0500

    fix

--- installation.sh
+++ installation.sh
@@ -1,4 +1,3 @@
-cp .env.example .env
 docker compose exec -T laravel.test  chmod -R 777 storage/ bootstrap/
 docker compose exec -T laravel.test  cp .env.example .env
 docker compose exec -T laravel.test  composer install
